dcff7f40fddf0d38c81b90a0ce2e44c7a26ffe26
Alejandro <alehl1998@gmail.com>
2022-05-12 17:37:02 -0600

    Ventana con espacioes de texto

--- AppInicializacion.py
+++ AppInicializacion.py
@@ -1,8 +1,31 @@
 import subprocess
+import tkinter as tk
 
+
+################ Conados #################
 comando = 'mkdir Prueba'
 
 def ejecutar(comandos):
     subprocess.run(comandos, shell=True)
 
-ejecutar(comando)
+#ejecutar(comando)
+
+
+##############      GUI     ##############
+
+window = tk.Tk()
+mensaje = tk.Label(text="Inserte tiempo de ejecución")
+mensaje.pack()
+
+entry1 = tk.Entry(fg='black',bg='white',width=20)
+entry1.pack()
+tiempoEjec = entry1.get()
+
+mensaje = tk.Label(text="Inserte tiempo de muestreo")
+mensaje.pack()
+
+entry1 = tk.Entry(fg='black',bg='white',width=20)
+entry1.pack()
+tiempoMues = entry1.get()
+
+window.mainloop()
