d69626c52aa23eb53e34a8e8a75669a708d0e39f
Alejandro <alehl1998@gmail.com>
2022-05-12 17:58:27 -0600

    GUI operacional

--- AppInicializacion.py
+++ AppInicializacion.py
@@ -14,18 +14,36 @@ def ejecutar(comandos):
 ##############      GUI     ##############
 
 window = tk.Tk()
+window.geometry('300x120')
 mensaje = tk.Label(text="Inserte tiempo de ejecución")
 mensaje.pack()
 
 entry1 = tk.Entry(fg='black',bg='white',width=20)
 entry1.pack()
-tiempoEjec = entry1.get()
 
 mensaje = tk.Label(text="Inserte tiempo de muestreo")
 mensaje.pack()
 
-entry1 = tk.Entry(fg='black',bg='white',width=20)
-entry1.pack()
-tiempoMues = entry1.get()
+entry2 = tk.Entry(fg='black',bg='white',width=20)
+entry2.pack()
+
+##def handle_keypress(event):
+##    """Print the character associated to the key pressed"""
+##    print(event.char)
+
+
+
+
+def handle_click(event):
+    tiempoEjec = entry1.get()
+    tiempoMues = entry2.get()
+
+button = tk.Button(text="Ejecutar")
+button.pack()
+
+button.bind("<Button-1>", handle_click)
+
+# Bind keypress event to handle_keypress()
+##window.bind("<Key>", handle_keypress)
 
 window.mainloop()
